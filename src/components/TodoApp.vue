9633cc45128d37ccb0b13b6ddeff52236658b647
Vivian Hayes <vivian.xw7@gmail.com>
2023-11-25 15:46:18 -0600

    update components

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TodoForm from './TotoForm.vue'
+import TodoList from './TodoList.vue'
+</script>
+
+<template>
+    <div id="todo-app">
+        <h1>To-Do List</h1>
+
+        <todo-form />
+        <todo-list />
+    </div>
+</template>
+
+<style scoped></style>
+
---
+++ src/stores/todoList.js
@@ -0,0 +1,9 @@
+import { defineStore } from 'pinia'
+
+export const useTodoListStore = defineStore( 'todoList', {
+    // state
+    // getters
+    //  actoins
+} )
+
+
